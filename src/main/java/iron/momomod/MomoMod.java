ab20300abe00a6661b510dbe7db2a29a38dc1f22
iron6271 <irononyt69@gmail.com>
2023-05-07 01:34:55 +0530

    test commit

--- src/main/java/iron/momomod/MomoMod.java
+++ src/main/java/iron/momomod/MomoMod.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+// i never knew java was so cool
 public class MomoMod implements ModInitializer {
     public static final String MOD_ID = "momo-mod";
     public static final Logger LOGGER = LoggerFactory.getLogger("momo-mod");
